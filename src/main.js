bd48ab2e542b569dcf080e0adf1c0b0cdf76d7ba
DebuggedMoon <predojevic@proton.me>
2024-12-29 21:41:45 +0100

    feat: add viewport border collisions checks

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vector2 from "./lib/Vector2.js";
 import RectangeEntity from "./RectangeEntity.js";
 
 const FRAMERATE = 1 / 30;
+const VIEWPORT_SIZE = new Vector2(
+	1920,
+	1080
+)
 
 /**
  * @type {HTMLCanvasElement}
@@ -18,6 +22,10 @@ const rectangle = new RectangeEntity(
 	new Vector2(200, 200)
 );
 
+const entities = [rectangle];
+
+console.log(rectangle, entities)
+
 function simulatePhysics() {
 
 	rectangle.simulate(FRAMERATE);
@@ -31,10 +39,60 @@ function drawFrame() {
 
 }
 
+/**
+ * 
+ * @param {RectangeEntity} entity 
+ */
+function doBoundingBoxCollisionCheck(entity) {
+
+	// console.log(entity, entity.getPosition)
+
+	const entityPosition = entity.getPosition();
+	const entitySize = entity.getSize();
+
+	const newPosition = {
+		x: entityPosition.x,
+		y: entityPosition.y
+	}
+
+	if (entityPosition.y + entitySize.y >= VIEWPORT_SIZE.y) {
+
+		newPosition.y = VIEWPORT_SIZE.y - entitySize.y;
+
+	} else if (entityPosition.y <= 0) {
+
+		newPosition.y = 0;
+		
+	}
+
+	if (entityPosition.x + entitySize.x >= VIEWPORT_SIZE.x) {
+
+		newPosition.x = VIEWPORT_SIZE.x - entitySize.x;
+		
+	} else if (entityPosition.x <= 0) {
+
+		newPosition.x = 0;
+
+	}
+
+	entity.setPosition(
+		new Vector2(
+			newPosition.x,
+			newPosition.y
+		)
+	);
+
+}
+
 setInterval(
 	() => {
 		simulatePhysics();
+
+		for (const entity of entities) {
+			doBoundingBoxCollisionCheck(entity)
+		}
+
 		drawFrame();
 	},
 	FRAMERATE * 1000
-)
+);
