60227b741615c6fc826434e43a954dad3ac28d37
DebuggedMoon <predojevic@proton.me>
2024-12-29 22:37:06 +0100

    feat: add PongGame class to manage game state

---
+++ src/PongGame.js
@@ -0,0 +1,188 @@
+import RectangeEntity from "./RectangeEntity.js";
+import Vector2 from "./lib/Vector2.js";
+
+const GameState = {
+	Initializing: Symbol("initializing"),
+	StartMenu: Symbol("start-menu"),
+	Playing: Symbol("playing"),
+	GameOver: Symbol("game-over")
+}
+
+export class PongGame {
+
+	/**
+	 * 
+	 * @param {HTMLCanvasElement} canvas 
+	 * @param {number} framerate 
+	 */
+	constructor(canvas, framerate) {
+
+		/**
+		 * @type {GameState}
+		 * @private
+		 */
+		this.state = GameState.Initializing
+
+		/**
+		 * @type {CanvasRenderingContext2D}
+		 * @private
+		 */
+		this.canvasContext = canvas.getContext("2d");
+
+		/**
+		 * @type {HTMLCanvasElement}
+		 * @private
+		 */
+		this.canvasElement = canvas;
+
+		/**
+		 * Target milliseconds per Frame
+		 * @type {number}
+		 * @readonly
+		 */
+		this.frameLength = 1000 / framerate
+
+		/**
+		 * @type {RectangeEntity}
+		 * @private
+		 */
+		this.playerEntity = new RectangeEntity(
+			new Vector2(200, 200), 
+			new Vector2(200, 200)
+		)
+
+		/**
+		 * @type {RectangeEntity[]}
+		 * @private
+		 */
+		this.rectangeEntities = [
+			this.playerEntity
+		];
+
+	}
+
+	/**
+	 * @returns {GameState}
+	 */
+	getGameState() {
+		return this.state;
+	}
+
+	/**
+	 * Checks whetere a rectangle entity is colliding with the viewport and handles it
+	 * @private
+	 */
+	_doViewportCollisions() {
+
+		for (const entity of this.rectangeEntities) {
+
+			const entityPosition = entity.getPosition();
+			const entitySize = entity.getSize();
+
+			const newPosition = {
+				x: entityPosition.x,
+				y: entityPosition.y
+			}
+
+			if (entityPosition.y + entitySize.y >= this.canvasElement.height) {
+
+				newPosition.y = this.canvasElement.height - entitySize.y;
+
+			} else if (entityPosition.y <= 0) {
+
+				newPosition.y = 0;
+				
+			}
+
+			if (entityPosition.x + entitySize.x >= this.canvasElement.width) {
+
+				newPosition.x = this.canvasElement.width - entitySize.x;
+				
+			} else if (entityPosition.x <= 0) {
+
+				newPosition.x = 0;
+
+			}
+
+			entity.setPosition(
+				new Vector2(
+					newPosition.x,
+					newPosition.y
+				)
+			);
+
+		}
+
+	}
+
+	initialize() {
+
+		this.startGame();
+
+	}
+
+	/**
+	 * @param {number} deltaTime 
+	 * @private
+	 */
+	_doSimulations(deltaTime) {
+
+		for (const entity of this.rectangeEntities) {
+
+			entity.simulate(deltaTime / 1000);
+			console.log("Simulating")
+	
+		}
+		
+	}
+
+	/**
+	 * @private
+	 */
+	_drawEntities() {
+
+		for (const entity of this.rectangeEntities) {
+
+			this.canvasContext.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
+			entity.render(this.canvasContext);	
+	
+		}
+
+	}
+
+	startGame() {
+
+		if (this.state == GameState.Playing) {
+			return;
+		}
+
+		/** @type {number} */
+		let lastFrameTime = this.frameLength;
+		this.state = GameState.Playing
+
+		console.log("Starting Game Loop");
+
+		(async () => {
+
+			console.log("Tick")
+			while (this.state == GameState.Playing) {
+
+				const frameStart = Date.now();
+	
+				this._doSimulations(lastFrameTime);
+				this._doViewportCollisions();
+				this._drawEntities();
+	
+				lastFrameTime = Math.max(
+					this.frameLength,
+					frameStart - Date.now()
+				)
+
+				await new Promise(resolve => setTimeout(resolve, this.frameLength - ( lastFrameTime % this.frameLength )));
+			}
+
+		})()
+
+	}
+
+}
--- src/main.js
+++ src/main.js
@@ -1,98 +1,11 @@
-import Vector2 from "./lib/Vector2.js";
-import RectangeEntity from "./RectangeEntity.js";
+import { PongGame } from "./PongGame.js";
 
-const FRAMERATE = 1 / 30;
-const VIEWPORT_SIZE = new Vector2(
-	1920,
-	1080
-)
+const FRAMERATE = 24;
 
 /**
  * @type {HTMLCanvasElement}
 */
 const gameCanvas = document.querySelector("#game-viewport");
+const pongGame = new PongGame(gameCanvas, FRAMERATE);
 
-/**
- * @type {CanvasRenderingContext2D}
-*/
-const canvasContext = gameCanvas.getContext("2d")
-
-const rectangle = new RectangeEntity(
-	new Vector2(200, 200), 
-	new Vector2(200, 200)
-);
-
-const entities = [rectangle];
-
-console.log(rectangle, entities)
-
-function simulatePhysics() {
-
-	rectangle.simulate(FRAMERATE);
-
-}
-
-function drawFrame() {
-
-	canvasContext.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
-	rectangle.render(canvasContext);
-
-}
-
-/**
- * 
- * @param {RectangeEntity} entity 
- */
-function doBoundingBoxCollisionCheck(entity) {
-
-	// console.log(entity, entity.getPosition)
-
-	const entityPosition = entity.getPosition();
-	const entitySize = entity.getSize();
-
-	const newPosition = {
-		x: entityPosition.x,
-		y: entityPosition.y
-	}
-
-	if (entityPosition.y + entitySize.y >= VIEWPORT_SIZE.y) {
-
-		newPosition.y = VIEWPORT_SIZE.y - entitySize.y;
-
-	} else if (entityPosition.y <= 0) {
-
-		newPosition.y = 0;
-		
-	}
-
-	if (entityPosition.x + entitySize.x >= VIEWPORT_SIZE.x) {
-
-		newPosition.x = VIEWPORT_SIZE.x - entitySize.x;
-		
-	} else if (entityPosition.x <= 0) {
-
-		newPosition.x = 0;
-
-	}
-
-	entity.setPosition(
-		new Vector2(
-			newPosition.x,
-			newPosition.y
-		)
-	);
-
-}
-
-setInterval(
-	() => {
-		simulatePhysics();
-
-		for (const entity of entities) {
-			doBoundingBoxCollisionCheck(entity)
-		}
-
-		drawFrame();
-	},
-	FRAMERATE * 1000
-);
+pongGame.initialize();
