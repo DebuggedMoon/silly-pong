699ca8905a21481616986ed1fa00eafdd83cb913
DebuggedMoon <predojevic@proton.me>
2025-01-01 13:58:07 +0100

    feat: add abstraction for EventListener API

---
+++ src/lib/BindableEvent.js
@@ -0,0 +1,103 @@
+
+/**
+ * @class
+ */
+export class BindableEventConnection {
+
+	/**
+	 * @param {BindableEvent} BindableEvent 
+	 * @public
+	 */
+	constructor(event) {
+
+		/**
+		 * @type {BindableEvent} 
+		 */
+		this.event = event
+
+	}
+
+	/**
+	 * @public
+	 */
+	disconnect() {
+
+		this.event.disconnect(this);
+		this.event = null
+	}
+
+}
+
+/**
+ * @class
+ */
+export class BindableEvent {
+
+	/**
+	 * 
+	 * @param {string} eventType 
+	 */
+	constructor(eventType) {
+
+		/**
+		 * @type {Map<BindableEventConnection, callback>}
+		 * @private
+		 */
+		this.connections = new Map();
+
+		/**
+		 * @type {string}
+		 */
+		this.eventType = eventType;
+	}
+
+	/**
+	 * @private
+	 */
+	_fireEvent(...args) {
+
+		for (const [connection, callback] of this.connections) {
+
+			try {
+				callback(...args);
+			} catch {
+				console.log(this, "failed to fire!")
+			}
+
+		}
+
+	}
+
+	/**
+	 * @param {(...) => void} callback 
+	 * @public
+	 */
+	connect(callback) {
+		
+		const eventConnection = new BindableEventConnection(this);
+		window.addEventListener(this.eventType, callback);
+		this.connections.set(eventConnection, callback);
+
+		return eventConnection;
+	}
+
+	/**
+	 * 
+	 * @param {BindableEventConnection} connection 
+	 * @public
+	 */
+	disconnect(connection) {
+		
+		const callback = this.connections.get(connection);
+
+		if (callback != null) {
+
+			window.removeEventListener(
+				this.eventType,
+				callback
+			)
+		}
+
+	}
+
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { BindableEvent } from "./lib/BindableEvent.js";
 import { PongGame } from "./PongGame.js";
 
 const FRAMERATE = 24;
@@ -7,5 +8,24 @@ const FRAMERATE = 24;
 */
 const gameCanvas = document.querySelector("#game-viewport");
 const pongGame = new PongGame(gameCanvas, FRAMERATE);
+
+const keydownEvent = new BindableEvent("keydown");
+const connection = keydownEvent.connect(
+	/**
+	 * 
+	 * @param {KeyboardEvent} input 
+	 */
+	input => {
+		console.log(input.key)
+	}
+)
+
+setTimeout(
+	() => {
+		connection.disconnect()
+		console.log("DICONNECTED")
+	},
+	5000
+)
 
 pongGame.initialize();
